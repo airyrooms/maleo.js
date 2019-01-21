f79ae41615713e25ad316b338a1947d8a7c77aad
Alvin Lim <alvink77@gmail.com>
2019-01-21 10:14:54 +0700

    fix(example): remove config js env production

--- example/playground/zones.config.js
+++ example/playground/zones.config.js
@@ -2,7 +2,5 @@ const tsPlugin = require('@airy//typescript-plugin');
 const cssPlugin = require('@airy//css-plugin');
 
 module.exports = tsPlugin(
-  cssPlugin({
-    env: 'production',
-  }),
+  cssPlugin(),
 );
