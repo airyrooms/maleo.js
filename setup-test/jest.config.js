1bee91b80b3c5e5479576538456ee2be8a737581
Alvin Lim <alvink77@gmail.com>
2019-01-23 15:50:28 +0700

    test(test): make babel-jest working
    Adding babelrc and @babel/plugin-transform-modules-commonjs to make babel-jest works

--- setup-test/jest.config.js
+++ setup-test/jest.config.js
@@ -50,18 +50,13 @@ module.exports = {
   // globalTeardown: null,
 
   // A set of global variables that need to be available in all test environments
-  // globals: {},
+  // globals: null,
 
   // An array of directory names to be searched recursively up from the requiring module's location
   moduleDirectories: ['node_modules'],
 
   // An array of file extensions your modules use
-  // moduleFileExtensions: [
-  //   "js",
-  //   "json",
-  //   "jsx",
-  //   "node"
-  // ],
+  moduleFileExtensions: ['js', 'json', 'jsx', 'node'],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
@@ -97,12 +92,10 @@ module.exports = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: 'test',
+  rootDir: process.cwd(),
 
   // A list of paths to directories that Jest should use to search for files in
-  // roots: [
-  //   "<rootDir>"
-  // ],
+  roots: ['<rootDir>/packages', '<rootDir>/packages/plugins'],
 
   // Allows you to use a custom runner instead of Jest's default test runner
   // runner: "jest-runner",
@@ -126,10 +119,10 @@ module.exports = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: ['**/*.test.js', '**/*.spec.js'],
+  testMatch: ['**/__test__/**/*.test.js', '**/__test__/**/*.spec.js'],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: ['/node_modules/'],
+  // testPathIgnorePatterns: ['/node_modules/'],
 
   // The regexp pattern Jest uses to detect test files
   // testRegex: "",
@@ -148,7 +141,7 @@ module.exports = {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.jsx?$': '<rootDir>/setup-test/transformer.js',
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
