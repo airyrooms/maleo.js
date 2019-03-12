c2efe306dd160dedb9d6bc99a241d374b8a869ea
Andarias Silvanus <andarias.silvanus@airyrooms.com>
2019-03-12 18:46:08 +0700

    feat(build, render): add html static export
    #10

--- packages/Maleo.js/src/utils/require.ts
+++ packages/Maleo.js/src/utils/require.ts
@@ -1,3 +1,5 @@
+// tslint:disable:no-console
+
 export const requireDynamic = async (path: string) => {
   // Has to use this kind of require because webpack tries to
   // bundle up normal require as webpack analyzes it as dynamic import
@@ -13,4 +15,14 @@ export const requireRuntime = (path: string) => {
     typeof __non_webpack_require__ !== 'undefined' ? __non_webpack_require__(path) : require(path);
 
   return typeof requiredModule.default !== 'undefined' ? requiredModule.default : requiredModule;
+};
+
+export const requireFile = (path: string) => {
+  try {
+    const file = requireRuntime(path);
+    return file;
+  } catch (error) {
+    console.log(`[ERROR] Error occured when require file ${path}`);
+    return null;
+  }
 };
