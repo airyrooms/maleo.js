e0d7f74ec8332c59f65f1b3038ff8a72f867d482
Alvin Lim <alvink77@gmail.com>
2019-03-18 11:25:04 +0700

    chore(publish-canary): change return to exit 1

--- publish-canary.sh
+++ publish-canary.sh
@@ -28,21 +28,22 @@ git status
 head_ref=$(git rev-parse HEAD)
 if [[ $? -ne 0 || ! $head_ref ]]; then
     err "failed to get HEAD reference"
-    return 1
+    exit 1
 fi
 branch_ref=$(git rev-parse "$TRAVIS_BRANCH")
 if [[ $? -ne 0 || ! $branch_ref ]]; then
     err "failed to get $TRAVIS_BRANCH reference"
-    return 1
+    exit 1
 fi
 if [[ $head_ref != $branch_ref ]]; then
     msg "HEAD ref ($head_ref) does not match $TRAVIS_BRANCH ref ($branch_ref)"
     msg "someone may have pushed new commits before this build cloned the repo"
-    return 0
+    exit 1
 fi
 if ! git checkout "$TRAVIS_BRANCH"; then
     err "failed to checkout $TRAVIS_BRANCH"
-    return 1
+    exit 1
+fi
 
 # Publish Script
 yarn publish:prepublish
