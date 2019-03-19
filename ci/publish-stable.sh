7e5481f71a3d331d0c3185f54da9c7e229d2e575
Alvin Kristanto <alvink77@gmail.com>
2019-03-19 15:59:47 +0700

    Revert "chore(publish-stable): manual publish (#95)"
    This reverts commit 0f1024f8cbae091348d341fa5ed5809a284b0bbd.

--- ci/publish-stable.sh
+++ ci/publish-stable.sh
@@ -1,5 +1,4 @@
 echo "Publishing Stable"
 yarn publish:prepublish
-# For now manually publish stable version for safety
-# yarn publish:version-stable
+yarn publish:version-stable
 yarn publish:stable
