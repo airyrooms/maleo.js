1243917ad416a90538e4f30e15938c7d75f41fdf
Alvin Kristanto <alvink77@gmail.com>
2019-03-20 15:51:36 +0700

    chore(travis): fix sync master canary (#117)
    do git fetch first then checkout to canary
    
    docs(contributing, sh): add publishing guideline on contributing.md

--- ci/publish-stable.sh
+++ ci/publish-stable.sh
@@ -1,5 +1,7 @@
 echo "Publishing Stable"
+echo "Running Prepublish"
 yarn publish:prepublish
 # publish stable manually
 # yarn publish:version-stable
+echo "Publishing Stable Version to NPM"
 yarn publish:stable
--- ci/travis-sync-master-canary.sh
+++ ci/travis-sync-master-canary.sh
@@ -1,4 +1,8 @@
 echo "Syncing Master to Canary"
+git fetch origin canary:canary
+echo "Checking out to canary"
 git checkout canary
+echo "Merging Master to Canary"
 git merge master -m 'Sync Master to Canary [skip ci]'
+echo "Pushing Sync"
 git push origin canary
