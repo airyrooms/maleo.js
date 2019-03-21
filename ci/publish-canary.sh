a95a7df6b849e71aff170fbb9ed522af4b98bce0
Alvin Kristanto <alvink77@gmail.com>
2019-03-21 12:33:55 +0700

    ci(travis): change webhook to verbose (#123)
    ci(circleci): fix config

--- ci/publish-canary.sh
+++ ci/publish-canary.sh
@@ -7,11 +7,11 @@ yarn publish:version-canary
 
 echo "Publishing Canary Version to NPM. More info $TRAVIS_BUILD_WEB_URL"
 if ! yarn publish:canary; then
-  wget https://raw.githubusercontent.com/DiscordHooks/travis-ci-discord-webhook/master/send.sh
+  wget https://raw.githubusercontent.com/DiscordHooks/travis-ci-discord-webhook/verbose/send.sh
   chmod +x send.sh
   ./send.sh failure $DISCORD_WEBHOOK_URL
 else
-  wget https://raw.githubusercontent.com/DiscordHooks/travis-ci-discord-webhook/master/send.sh
+  wget https://raw.githubusercontent.com/DiscordHooks/travis-ci-discord-webhook/verbose/send.sh
   chmod +x send.sh
   ./send.sh success $DISCORD_WEBHOOK_URL
 fi
