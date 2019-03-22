aa5ccc36f7aac85364a0633033548226256e735b
Alvin Lim <alvink77@gmail.com>
2019-03-22 15:35:02 +0700

    fix(ssr): fix component getinitialprops
    get initial props was undefined due to the page component was not preloaded yet

--- packages/Maleo.js/src/server/routeHandler.ts
+++ packages/Maleo.js/src/server/routeHandler.ts
@@ -1,6 +1,28 @@
 import { matchRoutes as reactRouterMatchRoutes } from 'react-router-config';
-import { AsyncRouteProps, Branch } from '@interfaces/render/IRender';
+import { AsyncRouteProps, Branch, PreBranch } from '@interfaces/render/IRender';
 
-export const matchingRoutes = (routes: AsyncRouteProps[], pathname: string): Branch[] => {
-  return reactRouterMatchRoutes(routes, pathname);
+export const matchingRoutes = async (
+  routes: AsyncRouteProps[],
+  pathname: string,
+): Promise<Branch[]> => {
+  const matchedRoutes: PreBranch[] = reactRouterMatchRoutes(routes, pathname);
+
+  const preloadedMatchedRoutes: Branch[] = [];
+
+  matchedRoutes.map(async (branch) => {
+    const { component } = branch.route;
+
+    // Preload Component route
+    const { default: preloadComponent } = await component.preload();
+
+    preloadedMatchedRoutes.push({
+      ...branch,
+      route: {
+        ...branch.route,
+        component: preloadComponent,
+      },
+    } as Branch);
+  });
+
+  return preloadedMatchedRoutes;
 };
