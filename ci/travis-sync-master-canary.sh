14e082cb9424aa890e4dfb9a91c80ec7441aeb6d
Alvin Kristanto <alvink77@gmail.com>
2019-03-23 15:21:02 +0700

    ci(travis): fix msg undefined and follow commit semantic sync (#137)
    fix #136

--- ci/travis-sync-master-canary.sh
+++ ci/travis-sync-master-canary.sh
@@ -1,5 +1,5 @@
-echo "Syncing Master to Canary"
+echo "chore: syncing master to canary"
 git fetch origin canary:canary
 git checkout canary
-git merge master -m 'Sync Master to Canary [skip ci]'
+git merge master -m 'chore: syncing master to canary [skip ci]'
 git push origin canary
