3dc1ddbff91f559776825fab0ad5dc0cfc4e94e7
Alvin Kristanto <alvink77@gmail.com>
2019-03-27 12:02:18 +0700

    docs(compose-plugins): example for using compose plugin (#159)

---
+++ example/compose-plugins/maleo.config.js
@@ -0,0 +1,18 @@
+const cssPlugin = require('@airy/maleo-css-plugin')
+const tsPlugin = require('@airy/maleo-typescript-plugin')
+const compose = require('@airy/maleo-compose-plugin')
+
+module.exports = compose([
+  [tsPlugin],
+  [cssPlugin, {
+    cssPluginOptions: {
+      extractCss: {
+        singleCssFile: true,
+      },
+      cssLoader: {
+        modules: true,
+        localIdentName: '[path][name]__[local]--[hash:base64:5]'
+      }
+    }
+  }]
+])
